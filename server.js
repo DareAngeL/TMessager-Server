ffcb68b726a5a30c5cf4386f293390ffc82d56d0
Rene Tajos Jr <master.isheart@gmail.com>
2022-10-21 16:32:12 +0800

    Update

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require('express')
 const http = require('http')
 const app = express()
-
 const server = http.createServer(app)
+const { Server } = require("socket.io")
+
+const socketIO = new Server(server)
 
 app.get('/', (req, res) => {
   res.send('Server is running...')
-  res.end()
 })
 
-const PORT = process.env.PORT || 1997
+socketIO.on('connection', (socket) => {
+  console.log('new user connected');
+})
+
+const PORT = process.env.PORT || 2022
 server.listen(PORT, () => {
   console.log('Listening on port ' + PORT)
 })
